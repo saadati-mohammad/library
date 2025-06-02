9327c77a2d1098a468e461594535c7055750c385
Pc <mohammadsaadati2003@gmail.com>
2025-06-02 18:39:06 +0330

    add length for description and add bookCoverFile field

--- src/main/java/ir/iau/library/entity/Book.java
+++ src/main/java/ir/iau/library/entity/Book.java
@@ -28,6 +28,7 @@ public class Book {
     private String publisher;
     private String isbn10;
     private String isbn13;
+    @Column(length = 2000)
     private String description;
     private String deweyDecimal;
     private String congressClassification;
@@ -37,12 +38,14 @@ public class Book {
     private LocalDate publicationDate;
     private Integer pageCount;
     private String language;
+    private byte[] bookCoverFile;
     private String edition;
     private Integer copyCount;
     private String librarySection; // بخش کتابخانه (مثلاً: علوم انسانی، مهندسی)
     private String shelfCode;      // کد یا شماره قفسه (مثلاً: A3, B5)
     private String rowNumber;      // ردیف (مثلاً: ردیف 2)
     private String columnNumber;   // ستون (مثلاً: ستون 4)
+    @Column(length = 2000)
     private String positionNote;   // توضیح اضافی درباره مکان یا شرایط خاص نگهداری
     private Boolean active; // soft delete flag
 
