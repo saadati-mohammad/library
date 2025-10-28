c2096600f80e5575fc62b5c070faed1cdb1a1404
TheWeeknd <itsmahdivafaei@gmail.com>
2025-10-28 15:01:49 +0330

    specify index.html for '' path

---
+++ src/main/java/ir/iau/library/controller/RouteController.java
@@ -0,0 +1,13 @@
+package ir.iau.library.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RouteController {
+
+    @RequestMapping(value = {"/{path:[^\\.]*}"})
+    public String forward() {
+        return "forward:/index.html";
+    }
+}
